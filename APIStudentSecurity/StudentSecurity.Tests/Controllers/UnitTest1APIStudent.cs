b87365dcccd215557547f78c5ba70b60c47ec1a7
Paul Aguilar Zegada <a2019113015@estudiantes.upsa.edu.bo>
2020-09-05 13:18:02 -0400

    Se creo el Unit Test del API Web Student, sólo el GET, falta POST, DELETE y PUT

---
+++ APIStudentSecurity/StudentSecurity.Tests/Controllers/UnitTest1APIStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using APIStudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSecurity.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1APIStudent
+    {
+        [TestMethod]
+        public void TestGetStudent()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController();
+
+            //Act
+            var listaEstudiantes = studentsController.GetStudents();
+
+            //Assert
+            Assert.IsNotNull(listaEstudiantes);
+        }
+    }
+}
